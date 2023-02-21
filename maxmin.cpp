16ee568de0a5ed84631b49d75ee6d69b287f4b12
Saksham1212 <95517838+Saksham1212@users.noreply.github.com>
2023-02-21 12:48:34 +0530

    Create maxmin.cpp

---
+++ maxmin.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+/* Driver program to test above function */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+   public:
+    int findSum(int A[], int N)
+    {
+    	//code here.
+   
+    	 int big=A[0];   
+     int small=A[0];
+    
+    
+   
+    
+    for(int i=0;i<N;i++){
+        if(A[i]>big){
+            big=A[i];
+            // continue;
+        }
+        if(A[i]<small){
+            small=A[i];
+            // continue;
+//           You can either add or not add continue here
+        }
+    }
+    return (big+small);
+    
+    
+    
+  }
+    
+    
+
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	      cin>>arr[i];
+	    Solution ob;  
+	    int ans=ob.findSum(arr, n);
+	    cout<<ans;
+	    cout<<"\n";
+	}
+	return 0;
+}
+
+// } Driver Code Ends
